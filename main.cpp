6794168334af6cd3e6c2b261595108e5dab273a2
En Shih <seanstone5923@gmail.com>
2019-05-10 18:30:37 +0800

    Add Lorentz transformation

--- main.cpp
+++ main.cpp
@@ -19,6 +19,14 @@ int main(int argc, char **argv)
 
 REAL rand1() { return 2 * (float)rand()/(float)(RAND_MAX) - 1; }
 
+vec4 lorentz_x(REAL eta, vec4 p)
+{
+    vec4 q = p;
+    q[0] = cosh(eta) * p[0] + sinh(eta) * p[1];
+    q[1] = sinh(eta) * p[0] + cosh(eta) * p[1];
+    return q;
+}
+
 void compute()
 {
     vec4 p;
@@ -27,9 +35,13 @@ void compute()
     p[3] = rand1();
     p[0] = sqrt(p[1]*p[1] + p[2]*p[2] + p[3]*p[3]);
 
-    int deci_places = 16;
+    int deci_places = 8;
     cout << setRwidth(deci_places + 8);
 
     cout << p << "\n";
     cout << p.norm() << "\n";
+
+    vec4 q = lorentz_x(1, p);
+    cout << q << "\n";
+    cout << q.norm() << "\n";
 }
